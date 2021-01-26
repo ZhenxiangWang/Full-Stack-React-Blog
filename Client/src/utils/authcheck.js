ab7026c22d1b78b7b4305ec901e4081ce4322158
Josh Wang <josh.wang@lmigroup.com>
2021-01-27 09:57:56 +1100

    feat: save user profile data to the database

--- Client/src/utils/authcheck.js
+++ Client/src/utils/authcheck.js
@@ -1,32 +1,37 @@
-import React, { useEffect, useContext } from 'react';
-import history from './history';
-import Context from './context';
-import * as ACTIONS from '../store/actions/actions';
+import React, { useEffect, useContext } from "react";
+import axios from "axios";
+import history from "./history";
+import Context from "./context";
 
-
+import * as ACTIONS from "../store/actions/actions";
 
 const AuthCheck = () => {
-  const context = useContext(Context)
+  const context = useContext(Context);
 
   useEffect(() => {
-    if(context.authObj.isAuthenticated()) {
-      context.handleUserLogin()
-      context.handleUserAddProfile(context.authObj.userProfile)
-      history.replace('/')
+    if (context.authObj.isAuthenticated()) {
+      const profile = context.authObj.userProfile;
+      context.handleUserLogin();
+      context.handleUserAddProfile(context.authObj.userProfile);
+      axios
+        .post("/api/posts/userprofiletodb", profile)
+        .then(
+          axios
+            .get("/api/get/userprofilefromdb", {
+              params: { email: profile.profile.email },
+            })
+            .then((res) => context.handleAddDBProfile(res.data))
+        )
+        .then(history.replace("/"));
+      history.replace("/");
+    } else {
+      context.handleUserLogout();
+      context.handleUserRemoveProfile();
+      history.replace("/");
     }
-    else {
-      context.handleUserLogout()
-      context.handleUserRemoveProfile()
-      history.replace('/')
-      }
-    }, [])
+  }, [context.authObj.userProfile, context]);
 
-    return(
-        <div>
-        </div>
-    )}
-
-
-
+  return <div></div>;
+};
 
 export default AuthCheck;
