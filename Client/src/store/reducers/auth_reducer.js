6a7603f3a40cac89ed51fa5e61baceade77d6973
Josh Wang <josh.wang@lmigroup.com>
2021-01-27 10:24:33 +1100

    feat: set up actions, reducers and context to setup the global state

--- Client/src/store/reducers/auth_reducer.js
+++ Client/src/store/reducers/auth_reducer.js
@@ -3,6 +3,7 @@ import * as ACTION_TYPES from "../actions/action_types";
 export const initialState = {
   is_authenticated: false,
   profile: null,
+  db_profile: null,
 };
 
 export const AuthReducer = (state = initialState, action) => {
